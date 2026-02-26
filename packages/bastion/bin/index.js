320c6ce03e32968837020d50512ade0e21cb0c46
Davide Di Criscito <dcdavidev@gmail.com>
2026-02-26 14:21:11 +0100

    chore(bastion-cli): add LICENSE and refine package metadata order

--- packages/bastion/bin/index.js
+++ packages/bastion/bin/index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 import { spawn } from 'node:child_process';
-import path from 'node:path';
 import os from 'node:os';
+import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
